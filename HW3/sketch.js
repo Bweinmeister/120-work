ecfe7da740d6ab0f09d83ab184da70ab1aa610b3
bweinmeister <35786995+Bweinmeister@users.noreply.github.com>
2018-02-08 16:42:03 -0700

    HW3

---
+++ HW3/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 400);
+  background('#fae');
+
+}
+// Declare the draw function
+function draw() {
+
+    // tell p5 to print the text string "Hello World!"
+    // This is going to be positioned at ( x: 20, y: 30 ).
+    text( "Hello World!", 20, 30 );
+}
